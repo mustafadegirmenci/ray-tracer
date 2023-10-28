c0762b4ff2af663ab888e3967824719ce164fde8
mustafadegirmenci <m.dgr@yahoo.com>
2023-10-28 12:57:21 +0300

    Add Sphere, Triangle intersections

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,48 +1,153 @@
-#include "parser.h"
 #include "raytracer.h"
+#include <vector>
+#include <cmath>
+#include <limits>
 
-RenderResult render() {
-    // The code below creates a test pattern and writes
-    // it to a PPM file to demonstrate the usage of the
-    // ppm_write function.
-    //
-    // Normally, you would be running your ray tracing
-    // code here to produce the desired image.
+#pragma region Vec3
 
-    const RGB BAR_COLOR[8] =
-            {
-                    { 255, 255, 255 },  // 100% White
-                    { 255, 255,   0 },  // Yellow
-                    {   0, 255, 255 },  // Cyan
-                    {   0, 255,   0 },  // Green
-                    { 255,   0, 255 },  // Magenta
-                    { 255,   0,   0 },  // Red
-                    {   0,   0, 255 },  // Blue
-                    {   0,   0,   0 },  // Black
-            };
+Vec3::Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
 
-    int width = 640, height = 480;
-    int columnWidth = width / 8;
+Vec3 Vec3::operator+(const Vec3& other) const {
+    return Vec3(x + other.x, y + other.y, z + other.z);
+}
 
-    unsigned char* image = new unsigned char[width * height * 3];
+Vec3 Vec3::operator-(const Vec3& other) const {
+    return Vec3(x - other.x, y - other.y, z - other.z);
+}
 
-    int i = 0;
-    for (int y = 0; y < height; ++y)
-    {
-        for (int x = 0; x < width; ++x)
-        {
-            int colIdx = x / columnWidth;
-            image[i++] = BAR_COLOR[colIdx][0];
-            image[i++] = BAR_COLOR[colIdx][1];
-            image[i++] = BAR_COLOR[colIdx][2];
+Vec3 Vec3::operator*(float scalar) const {
+    return Vec3(x * scalar, y * scalar, z * scalar);
+}
+
+Vec3 Vec3::operator/(float scalar) const {
+    return Vec3(x / scalar, y / scalar, z / scalar);
+}
+
+float Vec3::dot(const Vec3& other) const {
+    return x * other.x + y * other.y + z * other.z;
+}
+
+Vec3 Vec3::cross(const Vec3& other) const {
+    return Vec3(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
+}
+
+float Vec3::length() const {
+    return std::sqrt(x * x + y * y + z * z);
+}
+
+Vec3 Vec3::normalized() const {
+    float len = length();
+    return Vec3(x / len, y / len, z / len);
+}
+
+Ray::Ray(const Vec3& origin, const Vec3& direction) : origin(origin), direction(direction) {}
+
+#pragma endregion Vec3
+
+#pragma region Sphere
+Sphere::Sphere(const Vec3& center, float radius) : center(center), radius(radius) {}
+
+bool Sphere::intersect(const Ray& ray, float& t) const {
+    Vec3 oc = ray.origin - center;
+    float a = ray.direction.dot(ray.direction);
+    float b = 2.0f * oc.dot(ray.direction);
+    float c = oc.dot(oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0) {
+        float t1 = (-b - std::sqrt(discriminant)) / (2.0f * a);
+        float t2 = (-b + std::sqrt(discriminant)) / (2.0f * a);
+        t = (t1 < t2) ? t1 : t2;
+        return true;
+    }
+    return false;
+}
+
+#pragma endregion Sphere
+
+#pragma region Triangle
+Triangle::Triangle(const Vec3& v0, const Vec3& v1, const Vec3& v2) : v0(v0), v1(v1), v2(v2) {}
+
+bool Triangle::intersect(const Ray& ray, float& t) const {
+    Vec3 e1 = v1 - v0;
+    Vec3 e2 = v2 - v0;
+    Vec3 h = ray.direction.cross(e2);
+    float a = e1.dot(h);
+
+    if (a > -std::numeric_limits<float>::epsilon() && a < std::numeric_limits<float>::epsilon())
+        return false;
+
+    float f = 1.0f / a;
+    Vec3 s = ray.origin - v0;
+    float u = f * s.dot(h);
+
+    if (u < 0.0f || u > 1.0f)
+        return false;
+
+    Vec3 q = s.cross(e1);
+    float v = f * ray.direction.dot(q);
+
+    if (v < 0.0f || u + v > 1.0f)
+        return false;
+
+    t = f * e2.dot(q);
+
+    if (t > std::numeric_limits<float>::epsilon())
+        return true;
+
+    return false;
+}
+
+#pragma endregion Triangle
+
+#pragma region Tracing
+
+Vec3 trace(const Ray& ray, const std::vector<Sphere>& spheres, const std::vector<Triangle>& triangles) {
+    float tMin = std::numeric_limits<float>::max();
+    float t;
+    int closestSphere = -1;
+    int closestTriangle = -1;
+
+    // Find the closest intersection point with the spheres
+    for (int i = 0; i < spheres.size(); i++) {
+        if (spheres[i].intersect(ray, t) && t < tMin) {
+            tMin = t;
+            closestSphere = i;
+            closestTriangle = -1; // Reset the triangle intersection
         }
     }
 
-    // Create a RenderResult structure to hold the image image.
-    RenderResult result;
-    result.image = image;
-    result.width = width;
-    result.height = height;
+    // Find the closest intersection point with the triangles
+    for (int i = 0; i < triangles.size(); i++) {
+        if (triangles[i].intersect(ray, t) && t < tMin) {
+            tMin = t;
+            closestSphere = -1; // Reset the sphere intersection
+            closestTriangle = i;
+        }
+    }
 
+    if (closestSphere != -1) {
+        // Implement shading for spheres here
+        // For simplicity, we return the sphere color as is.
+        return Vec3(1, 0, 0); // Red sphere
+    }
+
+    if (closestTriangle != -1) {
+        // Implement shading for triangles here
+        // For simplicity, we return the triangle color as is.
+        return Vec3(0, 1, 0); // Green triangle
+    }
+
+    // If no intersection, return background color
+    return Vec3(0.7, 0.8, 1.0); // Blue sky
+}
+
+
+RenderResult render() {
+    // Implementation for rendering
+    // ...
+    RenderResult result;
     return result;
 }
+
+#pragma endregion Tracing
--- raytracer.h
+++ raytracer.h
@@ -1,9 +1,48 @@
+#ifndef RAYTRACER_H
+#define RAYTRACER_H
+
+#include <vector>
+
 typedef struct {
-    unsigned char *image;
+    unsigned char* image;
     int width;
     int height;
 } RenderResult;
 
 typedef unsigned char RGB[3];
 
+struct Vec3 {
+    float x, y, z;
+    Vec3(float x = 0, float y = 0, float z = 0);
+    Vec3 operator+(const Vec3& other) const;
+    Vec3 operator-(const Vec3& other) const;
+    Vec3 operator*(float scalar) const;
+    Vec3 operator/(float scalar) const;
+    float dot(const Vec3& other) const;
+    Vec3 cross(const Vec3& other) const;
+    float length() const;
+    Vec3 normalized() const;
+};
+
+struct Ray {
+    Vec3 origin, direction;
+    Ray(const Vec3& origin, const Vec3& direction);
+};
+
+struct Sphere {
+    Vec3 center;
+    float radius;
+    Sphere(const Vec3& center, float radius);
+    bool intersect(const Ray& ray, float& t) const;
+};
+
+struct Triangle {
+    Vec3 v0, v1, v2;
+    Triangle(const Vec3& v0, const Vec3& v1, const Vec3& v2);
+    bool intersect(const Ray& ray, float& t) const;
+};
+
+Vec3 trace(const Ray& ray, const std::vector<Sphere>& spheres, const std::vector<Triangle>& triangles);
 RenderResult render();
+
+#endif // RAYTRACER_H
