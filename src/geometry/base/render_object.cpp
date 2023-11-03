d4890e44e796305d9e21b7e1817c6ce16bf48e07
mustafadegirmenci <m.dgr@yahoo.com>
2023-11-03 14:52:34 +0300

    Fix include error

--- src/geometry/base/render_object.cpp
+++ src/geometry/base/render_object.cpp
@@ -1,5 +1,7 @@
-#include "../../include/geometry/render_object.h"
+#include "../../../include/utilities.h"
+#include "../../../include/geometry/base/render_object.h"
 
 Vec3f RenderObject::getNormal(const Scene& scene, const Vec3f& intersectionPoint) {
-
+    //TODO
+    return Vec3f(0, 0, 0);
 }
